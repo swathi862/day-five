9d518dd26f429b6e975b7ebeb4a43f41528addd4
Swathi Mukkamala <swathikmukkamala@gmail.com>
2020-03-21 11:20:09 -0400

    lightning exercises

---
+++ script.js
@@ -0,0 +1,22 @@
+var likesToTravel = true;
+
+if (likesToTravel === true){
+    console.log("Bon Voyage!");
+}
+else{
+    console.log("Enjoy our couch!");
+}
+
+// == means that type doesn't matter 2+2 = "4" returns true
+
+var favoriteFood = "ice-cream";
+
+if (favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls"){
+    console.log("Congratulations! You have excellent taste!");
+}
+// else if (favoriteFood === "ice-cream"){
+//     console.log("I scream, You scream, We all scream for ice-cream!!!")
+// }
+else{
+    console.log("Clearly, you have not tried fried pickles or pepperoni rolls.")
+}
